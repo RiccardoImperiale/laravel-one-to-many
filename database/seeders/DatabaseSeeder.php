1c909ba2cbd110811551b87d92cc3b2826617ae1
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-23 12:37:25 +0200

    add types table migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([ProjectSeeder::class]);
+        $this->call(
+            [
+                ProjectSeeder::class,
+                TypeSeeder::class
+            ]
+        );
     }
 }
